b7350f118d5fb69c9195f052d06602dc6169ed49
Michael Schwab <mike.schwab@gmail.com>
2008-07-11 13:06:12 -0400

    added plan of attack to readme

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'rubygems'
 require 'merb-core'
 require 'merb-core/test'
-require 'merb_activerecord'
+#require 'merb_activerecord'
 require 'merb_xss_terminate'
 
 Merb.start :adapter => 'runner', :environment => 'test', :merb_root => File.dirname(__FILE__)
