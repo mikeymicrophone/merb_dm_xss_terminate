37cc4c11900c6a45dfd0d48955cc174e83a8f496
Michael Schwab <mike.schwab@gmail.com>
2008-07-11 15:58:42 -0400

    working on inclusion logic

--- lib/merb_dm_xss_terminate.rb
+++ lib/merb_dm_xss_terminate.rb
@@ -3,9 +3,9 @@ if defined?(Merb::Plugins)
   require 'merb_dm_xss_terminate/rails_sanitize'
   require 'merb_dm_xss_terminate/html5lib_sanitize'
 
-  Merb::BootLoader.before_app_loads do
-    DataMapper::Resource.send(:include, XssTerminate)
-  end
+  # Merb::BootLoader.before_app_loads do
+  #   DataMapper::Resource.send(:include, XssTerminate)
+  # end
 
   Merb::Plugins.add_rakefiles "merb_dm_xss_terminate/merbtasks"
 end
