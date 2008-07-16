635d30e68c200081362c9c30c28bc2d2caa2bd61
Michael Schwab <mike.schwab@gmail.com>
2008-07-16 16:41:57 -0400

    updated setting of inheritable defaults for sanitization

--- lib/merb_dm_xss_terminate/xss_terminate.rb
+++ lib/merb_dm_xss_terminate/xss_terminate.rb
@@ -9,12 +9,12 @@ module XssTerminate
     def xss_terminate(options = {})
       before :save, :sanitize_fields
 
-      write_inheritable_attribute(:xss_terminate_options, {
+      self.xss_terminate_options = {
         :disable => (options[:disable] || false),
         :except => (options[:except] || []),
         :html5lib_sanitize => (options[:html5lib_sanitize] || []),
         :sanitize => (options[:sanitize] || [])
-      })
+      }
       
       class_inheritable_reader :xss_terminate_options
 
