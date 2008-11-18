b5fb89eba5e03e49f56624c705515ff20932c4ca
Mike Schwab <mike.schwab@gmail.com>
2008-11-18 16:07:03 -0500

    maybe this rake task for sanitizing existing data will work

--- lib/merb_dm_xss_terminate/merbtasks.rb
+++ lib/merb_dm_xss_terminate/merbtasks.rb
@@ -1,9 +1,9 @@
-namespace :merb_xss_terminate do
+namespace :merb_dm_xss_terminate do
   namespace :db do
 
     desc "Given MODELS=Foo,Bar,Baz find all instances in the DB and save to sanitize existing records"
     task :sanitize => :environment do
-      models = ENV['MODELS'].split(',')
+      models = Dir.open(Merb.root + '/app/models').reject { |file_name| ['.', '..'].include? file_name }.map { |file_name| file_name.gsub(/\.rb/, '').gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } }
       models.each do |model|
         Module.const_get(model).send(:all).map { |record| record.save }
       end
